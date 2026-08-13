947a6a30ed55f3c73eb4b80997a00ea4f8036eef
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-13 18:45:18 +0800

    export node-25 tool gateway runtime

--- services/tool-gateway/src/lumi_tool_gateway/__init__.py
+++ services/tool-gateway/src/lumi_tool_gateway/__init__.py
@@ -1,2 +1,67 @@
+from .audit import MemoryAuditSink, NullAuditSink, ToolAuditRecord, redact_arguments
+from .catalog import build_p0_registry, p0_tool_definitions
+from .contracts import (
+    ApprovalDecision,
+    ToolAdapterOutput,
+    ToolApproval,
+    ToolCallStatus,
+    ToolDefinition,
+    ToolIdempotency,
+    ToolPermissionContext,
+    ToolRequest,
+    ToolResult,
+    ToolRisk,
+    ToolRuntime,
+    ToolSideEffectContext,
+    ToolSideEffectResponse,
+)
+from .gateway import ToolGateway
+from .native import (
+    HTTPTransportResponse,
+    NativeFunctionAdapter,
+    SafeWebFetchAdapter,
+    SandboxExecuteAdapter,
+    WebSearchAdapter,
+)
+from .policy import ToolApprovalPolicy, ToolPermissionPolicy, ToolPolicyDecision
+from .registry import ToolRegistry
+from .schema import SchemaValidator
+from .ssrf import SSRFPolicy, ValidatedTarget
+
 SERVICE_NAME = "tool-gateway"
-VERSION = "0.0.0-dev"
+VERSION = "1.0.0-node25"
+
+__all__ = [
+    "ApprovalDecision",
+    "HTTPTransportResponse",
+    "MemoryAuditSink",
+    "NativeFunctionAdapter",
+    "NullAuditSink",
+    "SSRFPolicy",
+    "SafeWebFetchAdapter",
+    "SandboxExecuteAdapter",
+    "SchemaValidator",
+    "ToolAdapterOutput",
+    "ToolApproval",
+    "ToolApprovalPolicy",
+    "ToolAuditRecord",
+    "ToolCallStatus",
+    "ToolDefinition",
+    "ToolGateway",
+    "ToolIdempotency",
+    "ToolPermissionContext",
+    "ToolPermissionPolicy",
+    "ToolPolicyDecision",
+    "ToolRegistry",
+    "ToolRequest",
+    "ToolResult",
+    "ToolRisk",
+    "ToolRuntime",
+    "ToolSideEffectContext",
+    "ToolSideEffectResponse",
+    "ValidatedTarget",
+    "WebSearchAdapter",
+    "build_p0_registry",
+    "p0_tool_definitions",
+    "redact_arguments",
+]
