bd228a7894b3d5bc762af63a8761ef60c8d86e81
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-15 00:29:25 +0800

    NODE-50: export quality observability projection

--- packages/quality-engine/src/index.ts
+++ packages/quality-engine/src/index.ts
@@ -2,6 +2,7 @@ export * from "./artifact-adapter";
 export * from "./calibration";
 export * from "./deterministic";
 export * from "./engine";
+export * from "./observability";
 export * from "./ports";
 export * from "./profiles";
 export * from "./types";
