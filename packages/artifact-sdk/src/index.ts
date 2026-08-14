bea24f6a9dc2d22f214064139d4f2d634035f26b
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-14 17:43:27 +0800

    NODE-42 export artifact runtime

--- packages/artifact-sdk/src/index.ts
+++ packages/artifact-sdk/src/index.ts
@@ -1,5 +1,5 @@
-/** NODE-02 bootstrap marker. Artifact contracts land in NODE-15/NODE-42. */
-export interface ArtifactSdkBootstrap {
-  readonly package: "@lumi/artifact-sdk";
-  readonly version: "0.0.0";
-}
+export * from "./engine";
+export * from "./export";
+export * from "./gc";
+export * from "./hashing";
+export * from "./types";
