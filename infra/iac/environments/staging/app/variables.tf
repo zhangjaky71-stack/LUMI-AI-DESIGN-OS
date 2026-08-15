e9a39075ca57fac46aa5a098be8d76fc7f83977d
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-15 21:30:52 +0800

    feat(iac): define staging app deployment inputs

---
+++ infra/iac/environments/staging/app/variables.tf
@@ -0,0 +1,16 @@
+variable "account_id" { type = string }
+variable "region" { type = string }
+variable "core_state_bucket" { type = string }
+variable "core_state_key" { type = string, default = "lumi/staging/core/terraform.tfstate" }
+variable "certificate_arn" { type = string }
+variable "domain_name" { type = string }
+variable "hosted_zone_id" { type = string }
+
+variable "api_image" { type = string }
+variable "agent_runtime_image" { type = string }
+variable "model_gateway_image" { type = string }
+variable "tool_gateway_image" { type = string }
+variable "worker_media_image" { type = string }
+variable "sandbox_runtime_image" { type = string }
+
+variable "waf_rate_limit_requests_per_5m" { type = number, default = 1000 }
