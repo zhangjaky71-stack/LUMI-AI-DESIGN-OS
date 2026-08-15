bb2d38b75700c5cea41a66c33e1bb8543faea84d
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-15 20:59:30 +0800

    fix(iac): include public subnets for ALB only

--- infra/iac/modules/compute/variables.tf
+++ infra/iac/modules/compute/variables.tf
@@ -1,6 +1,7 @@
 variable "project" { type = string }
 variable "environment" { type = string }
 variable "vpc_id" { type = string }
+variable "public_subnet_ids" { type = list(string) }
 variable "private_subnet_ids" { type = list(string) }
 variable "app_security_group_id" { type = string }
 variable "alb_security_group_id" { type = string }
