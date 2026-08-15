17bb7b6617d63edab29e014886c1ec37f880ebc6
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-15 21:58:48 +0800

    chore(iac): pin staging migration toolchain

--- infra/iac/environments/staging/migration/versions.tf
+++ infra/iac/environments/staging/migration/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.8.0, < 2.0.0"
+  required_version = ">= 1.14.6, < 1.15.0"
   backend "s3" {}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0, < 7.0"
+      version = "= 6.55.0"
     }
   }
 }
