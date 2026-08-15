d46727e50b41974c418786520450f03de525acb4
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-15 20:56:46 +0800

    feat(iac): expose network module outputs

---
+++ infra/iac/modules/network/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = [for az in var.availability_zones : aws_subnet.public[az].id]
+}
+
+output "private_subnet_ids" {
+  value = [for az in var.availability_zones : aws_subnet.private[az].id]
+}
+
+output "data_subnet_ids" {
+  value = [for az in var.availability_zones : aws_subnet.data[az].id]
+}
+
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
+}
+
+output "app_security_group_id" {
+  value = aws_security_group.app.id
+}
