41957ddc5d27a63714964a3379577cc07380c22d
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-16 15:50:13 +0800

    feat(projects): export project core package

---
+++ apps/api/src/lumi_api/projects/__init__.py
@@ -0,0 +1,38 @@
+from .models import (
+    BriefVersion,
+    DataRetentionProfile,
+    DefaultProjectBranch,
+    ProjectBrief,
+    ProjectCommandError,
+    ProjectEvent,
+    ProjectEventType,
+    ProjectListQuery,
+    ProjectPage,
+    ProjectRecord,
+    ProjectSettings,
+    ProjectSummary,
+    QualityProfile,
+)
+from .service import ProjectCoreService, ProjectCreateCommand, ProjectPatchCommand
+from .store import MemoryProjectRepository, ProjectRepository
+
+__all__ = [
+    "BriefVersion",
+    "DataRetentionProfile",
+    "DefaultProjectBranch",
+    "MemoryProjectRepository",
+    "ProjectBrief",
+    "ProjectCommandError",
+    "ProjectCoreService",
+    "ProjectCreateCommand",
+    "ProjectEvent",
+    "ProjectEventType",
+    "ProjectListQuery",
+    "ProjectPage",
+    "ProjectPatchCommand",
+    "ProjectRecord",
+    "ProjectRepository",
+    "ProjectSettings",
+    "ProjectSummary",
+    "QualityProfile",
+]
