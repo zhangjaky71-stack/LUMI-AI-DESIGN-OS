e6f3ab7c5989cdc7c79731e196d050eea643c0a9
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-18 07:29:39 +0800

    feat(critic): expose NODE-50 service API

--- services/visual-critic/src/lumi_visual_critic/__init__.py
+++ services/visual-critic/src/lumi_visual_critic/__init__.py
@@ -1,2 +1,50 @@
 SERVICE_NAME = "visual-critic"
-VERSION = "0.0.0-dev"
+VERSION = "1.0.0"
+
+from .engine import QualityOperationConflict, VisualCriticEngine
+from .model import (
+    ArtifactQualityInput,
+    DimensionAssessment,
+    EvidenceKind,
+    GraderCalibrationSnapshot,
+    QualityDimension,
+    QualityEvidence,
+    QualityGateStatus,
+    QualityProfileKey,
+    QualityProfileSnapshot,
+    QualityResult,
+    QualitySeverity,
+    QualitySignalBundle,
+    QualityTaskSpec,
+    QualityViolation,
+    RepairAction,
+    RepairActionType,
+    VisualGraderResult,
+)
+from .profiles import BUILTIN_PROFILES, get_builtin_profile
+from .repository import InMemoryQualityResultRepository
+
+__all__ = [
+    "ArtifactQualityInput",
+    "BUILTIN_PROFILES",
+    "DimensionAssessment",
+    "EvidenceKind",
+    "GraderCalibrationSnapshot",
+    "InMemoryQualityResultRepository",
+    "QualityDimension",
+    "QualityEvidence",
+    "QualityGateStatus",
+    "QualityOperationConflict",
+    "QualityProfileKey",
+    "QualityProfileSnapshot",
+    "QualityResult",
+    "QualitySeverity",
+    "QualitySignalBundle",
+    "QualityTaskSpec",
+    "QualityViolation",
+    "RepairAction",
+    "RepairActionType",
+    "VisualCriticEngine",
+    "VisualGraderResult",
+    "get_builtin_profile",
+]
