6bb2c10644e959348e6e278abf96f76d13878418
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-18 00:34:09 +0800

    feat(export): add NODE-49 migration rollback

---
+++ apps/api/migrations/versions/20260817_0018_sql/down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS export_download_grants;
+-- statement-breakpoint
+DROP TABLE IF EXISTS export_outputs;
+-- statement-breakpoint
+DROP TABLE IF EXISTS export_items;
+-- statement-breakpoint
+DROP TABLE IF EXISTS export_jobs;
+-- statement-breakpoint
+DROP TABLE IF EXISTS export_specs;
