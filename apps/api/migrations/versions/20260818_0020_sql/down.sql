8f730fe7f2e9743d4447cb19b5c546cbd0b79ec7
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-18 08:05:02 +0800

    feat(repair): add NODE-51 migration rollback

---
+++ apps/api/migrations/versions/20260818_0020_sql/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS repair_learning_signals;
+-- statement-breakpoint
+DROP TABLE IF EXISTS auto_repair_attempts;
+-- statement-breakpoint
+DROP TABLE IF EXISTS auto_repair_jobs;
+-- statement-breakpoint
+DROP TABLE IF EXISTS repair_policy_snapshots;
