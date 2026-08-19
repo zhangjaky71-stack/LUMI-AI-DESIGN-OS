9329b7ccdcb2fd511601617ad96c87afc24bb467
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-19 17:25:43 +0800

    release: expose production RDS recovery outputs

--- infra/iac/environments/production/core/outputs.tf
+++ infra/iac/environments/production/core/outputs.tf
@@ -12,6 +12,10 @@ output "bucket_names" { value = module.platform_core.bucket_names }
 output "secret_arns" { value = module.platform_core.secret_arns }
 output "postgres_endpoint" { value = module.platform_core.postgres_endpoint }
 output "postgres_port" { value = module.platform_core.postgres_port }
+output "postgres_instance_id" { value = module.platform_core.postgres_instance_id }
+output "postgres_backup_retention_days" { value = module.platform_core.postgres_backup_retention_days }
+output "postgres_db_subnet_group_name" { value = module.platform_core.postgres_db_subnet_group_name }
+output "postgres_security_group_id" { value = module.platform_core.postgres_security_group_id }
 output "postgres_master_secret_arn" { value = module.platform_core.postgres_master_secret_arn, sensitive = true }
 output "redis_primary_endpoint" { value = module.platform_core.redis_primary_endpoint }
 output "rabbitmq_instances" { value = module.platform_core.rabbitmq_instances, sensitive = true }
