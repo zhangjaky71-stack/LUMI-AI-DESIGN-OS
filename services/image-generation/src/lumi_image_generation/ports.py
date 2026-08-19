e85e165a1ee9cb2487797764eaf984b4fe5f82f6
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-19 10:30:08 +0800

    refactor(node-46): make reference authorization async

--- services/image-generation/src/lumi_image_generation/ports.py
+++ services/image-generation/src/lumi_image_generation/ports.py
@@ -59,7 +59,7 @@ class PendingInvocationRecord:
 
 
 class ReferenceAuthorizationPort(Protocol):
-    def authorize(
+    async def authorize(
         self,
         spec: ImageGenerationSpec,
         references: tuple[ImageReference, ...],
