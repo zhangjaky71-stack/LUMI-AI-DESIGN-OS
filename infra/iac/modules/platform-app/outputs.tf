e96e935c37313b0b4191fc8cbd05c84eaf69dedc
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-21 07:42:13 +0800

    iac: propagate expected ecs service capacity

--- infra/iac/modules/platform-app/outputs.tf
+++ infra/iac/modules/platform-app/outputs.tf
@@ -1,5 +1,6 @@
 output "cluster_arn" { value = module.compute.cluster_arn }
 output "service_names" { value = module.compute.service_names }
+output "service_desired_counts" { value = module.compute.service_desired_counts }
 output "task_role_arns" { value = module.compute.task_role_arns }
 output "alb_arn" { value = module.compute.alb_arn }
 output "alb_dns_name" { value = module.compute.alb_dns_name }
