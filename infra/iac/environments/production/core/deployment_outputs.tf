ee7629098794970157da1bc8e71b378427dd8ba3
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-22 17:41:31 +0800

    fix(iac): remove duplicate production deployment outputs

--- infra/iac/environments/production/core/deployment_outputs.tf
+++
@@ -1,2 +0,0 @@
-output "postgres_instance_id" { value = module.platform_core.postgres_instance_id }
-output "postgres_backup_retention_days" { value = module.platform_core.postgres_backup_retention_days }
